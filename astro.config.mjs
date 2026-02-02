ce4e4bcfbd49bccc41060389294d4f6a09b071c1
yohannaftali <yohan.naftali@ptsenopati.com>
2026-02-02 09:16:32 +0700

    Build your first Astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astrohelloworld-yohannaftali.netlify.app/"
+  site: "https://astrohelloworld-yohannaftali.netlify.app/",
+  integrations: [preact()]
 });
